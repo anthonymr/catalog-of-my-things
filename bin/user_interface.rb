b59f10377043f82156f6441ac5ce4c629bb55fce
AbdimulhinYussuf3675 <adamabdimulhi.001@gmail.com>
2023-04-24 17:18:06 +0300

    user with a list of options to perform

---
+++ bin/user_interface.rb
@@ -0,0 +1,18 @@
+class UserInterface
+    def options
+        puts 'Please choose an options'
+        puts '1 - List all books'
+        puts '2 - List all music albums'
+        puts '3 - List all movie'
+        puts '4 - List all genres (e.g "Comedy", "Thriller")'
+        puts '5 - List all labels (e.g. "Gift", "New")'
+        puts '6 - List all authors (e.g. "Stephen King")'
+        puts '7 - List all sources (e.g. "From a friend", "Online shop")'
+        puts '8 - Add a book'
+        puts '9 - Add a music album'
+        puts '10 - Add a movie'
+        puts '11 - Add a game'
+        puts '12- EXIT'
+        print 'Enter the option: '
+    end
+end
