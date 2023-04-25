07d3b070646c9ea398de8962c86e7d54a62205b5
AbdimulhinYussuf3675 <adamabdimulhi.001@gmail.com>
2023-04-25 09:44:55 +0300

    Solve linter error

--- bin/games.rb
+++ bin/games.rb
@@ -1,32 +1,22 @@
 require_relative 'item'
 
 class Game < Item
-    attr_accessor :multiplayer, :last_played_at
-    attr_reader :id
+  attr_accessor :multiplayer, :last_played_at
+  attr_reader :id
 
-    def initialize(id, publish_date, archived, multiplayer, last_played_at)
-        super(id, publish_date, archived: archived)
-        @multiplayer = multiplayer
-        @last_played_at = last_played_at
-    end
+  def initialize(id, publish_date, archived, multiplayer, last_played_at)
+    super(id, publish_date, archived: archived)
+    @multiplayer = multiplayer
+    @last_played_at = last_played_at
+  end
 
-    private
+  private
 
-    def can_be_archived?
-        today_date = Date.today.strftime('%Y-%m-%d')
-        parsed = Date.parse(today_date)
-        last_played_date = Date.parse(@last_played_at)
-
-        difference_in_years = (parse.year - last_played_date.year).to_i
-
-        super && (difference_in_years > 2)
-    end
-
-    def can_be_archived?
-        today_date = Date.today.strftime('%Y-%m-%d')    
-        parsed = Date.parse(today_date)    
-        last_played_date = Date.parse(@last_played_at)    
-        difference_in_years = (parsed.year - last_played_date.year).to_i
-        super && (difference_in_years > 2)
-      end
+  def can_be_archived?
+    today_date = Date.today.strftime('%Y-%m-%d')
+    parsed = Date.parse(today_date)
+    last_played_date = Date.parse(@last_played_at)
+    difference_in_years = (parsed.year - last_played_date.year).to_i
+    super && (difference_in_years > 2)
+  end
 end
