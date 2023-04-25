0195992cf4665b8c798db7c525d7f87d2e2c965c
AbdimulhinYussuf3675 <adamabdimulhi.001@gmail.com>
2023-04-25 14:20:27 +0300

    Add options for author and game classes

--- bin/app.rb
+++ bin/app.rb
@@ -1,9 +1,13 @@
 require_relative 'book'
+require_relative 'game'
+require_relative 'author'
 
 class App
   def initialize
     @books = []
     @labels = []
+    @games = []
+    @authors = []
   end
 
   def add_book(publisher, cover_state, publish_date)
@@ -18,4 +22,18 @@ class App
   def list_labels
     @labels.map(&:title).join(', ')
   end
+
+  def add_game(multiplayer, last_played_at, publish_date)
+    game = Game.new(multiplayer, last_played_at, publish_date)
+    @games << game
+  end
+
+  def list_games
+    @games.each { |game| puts game }
+  end
+
+  def list_author    
+    @authors.each (&:first_name).join(', ')
+  end
+
 end
