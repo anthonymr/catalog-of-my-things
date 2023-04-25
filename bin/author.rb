0d207b72169c00c783609190923e9c6801db7b82
AbdimulhinYussuf3675 <adamabdimulhi.001@gmail.com>
2023-04-25 11:52:06 +0300

    Implement changes

--- bin/author.rb
+++ bin/author.rb
@@ -1,13 +1,15 @@
-require_relative 'item'
-
 class Authour
-  attr_accessor :first_name, :last_name, :items
-  attr_reader :id
+  attr_accessor :first_name, :last_name
 
-  def initialize(_id, first_name, last_name)
+  def initialize(first_name, last_name)
     @id = Random.rand(1...1000)
     @first_name = first_name
     @last_name = last_name
     @items = []
+  end
+
+  def add_item(item)
+    @items << item
+    item.author = self
   end
 end
