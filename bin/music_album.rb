cee632c932064876a33dba76e19e4bbda8218016
Okolo Raphael <okolonnaemeka2017@gmail.com>
2023-04-25 09:22:27 +0100

    Remove name, and publish_date

--- bin/music_album.rb
+++ bin/music_album.rb
@@ -1,12 +1,9 @@
 require_relative 'item'
 class MusicAlbum < Item
   attr_accessor :on_spotify
-  attr_reader :publish_date
 
   def initialize(on_spotify)
     super(publish_date)
-    @name = name
-    @publish_date = publish_date
     @on_spotify = on_spotify
   end
 
