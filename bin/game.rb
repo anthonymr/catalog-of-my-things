5cad1df8ae421ce62aed4afee0ade894e568f834
AbdimulhinYussuf3675 <adamabdimulhi.001@gmail.com>
2023-04-25 11:49:35 +0300

    Implement the changes

---
+++ bin/game.rb
@@ -0,0 +1,13 @@
+require_relative 'item'
+
+class Game < Item
+  attr_accessor :multiplayer, :last_played_at
+
+  def initialize(publish_date, multiplayer, last_played_at)
+    super(publish_date)
+    @multiplayer = multiplayer
+    @last_played_at = last_played_at
+  end
+
+  can_be_archived?
+end
