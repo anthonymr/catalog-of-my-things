e879b450e67c8e0aa71b86c9fa511f5cf58638dc
Okolo Raphael <okolonnaemeka2017@gmail.com>
2023-04-25 07:09:29 +0100

    Fix the typo error

--- bin/music_album.rb
+++ bin/music_album.rb
@@ -3,10 +3,10 @@ class MusicAlbum < Item
   attr_accessor :on_spotify, :name
   attr_reader :publish_date
 
-  def initialize(name, publish_date, on_spotify)
-    super(publish_date)
+  def initialize(name, on_spotify)
+     super(publish_date)
     @name = name
-    @publised_date = publised_date
+    @publish_date = publish_date
     @on_spotify = on_spotify
   end
 
