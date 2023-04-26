7ab9612b9a8a35e6195717165c9664d077655251
AbdimulhinYussuf3675 <adamabdimulhi.001@gmail.com>
2023-04-26 11:44:09 +0300

    Update class for data storage

--- bin/author.rb
+++ bin/author.rb
@@ -1,7 +1,10 @@
 class Author
   attr_accessor :first_name, :last_name
+  def self.from_hash(data)
+    new(data['first_name'], data['last_name'], data['id'])
+  end
 
-  def initialize(first_name, last_name)
+  def initialize(first_name, last_name, id = nil)
     @id = Random.rand(1...1000)
     @first_name = first_name
     @last_name = last_name
@@ -11,5 +14,14 @@ class Author
   def add_item(item)
     @items << item unless @items.include?(item)
     item.author = self
+  end
+
+  def to_hash
+    {
+      id: @id,
+      first_name: @first_name,
+      last_name: @last_name,
+      author_id: @author&.id
+    }
   end
 end
