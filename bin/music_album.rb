707e49249a057a8d7441423e063342db3d6fb74c
Okolo Raphael <okolonnaemeka2017@gmail.com>
2023-04-26 09:19:45 +0100

    Add to_hash method and to_s method

--- bin/music_album.rb
+++ bin/music_album.rb
@@ -2,18 +2,31 @@ require_relative 'item'
 class MusicAlbum < Item
   attr_accessor :on_spotify
 
-  def initialize(on_spotify, publish_date)
-    super(publish_date)
+  def self.from_hash(data)
+    new(data['on_spotify'], data['publish_date'], data['id'])
+  end
+
+  def initialize(on_spotify, publish_date, id = nil)
+    super(publish_date, id)
     @on_spotify = on_spotify
+  end
+
+  def to_s
+    "(#{@id}) on spotify: #{@on_spotify} - publish date: #{@publish_date.to_date}"
+  end
+
+  def to_hash
+    {
+      id: @id,
+      publish_date: @publish_date,
+      on_spotify: @on_spotify,
+      genre_id: @genre&.id
+    }
   end
 
   private
 
   def can_be_archived?
     super && @on_spotify
-  end
-
-  def to_s
-    "(#{@id}) on spotify: #{@on_spotify} - publish date: #{@publish_date.to_date}"
   end
 end
