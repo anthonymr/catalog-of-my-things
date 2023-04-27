08d66becbc38756df709b9337b751a5cf792d778
AbdimulhinYussuf3675 <adamabdimulhi.001@gmail.com>
2023-04-27 16:06:02 +0300

    Upadate preserved data

--- bin/local_storage.rb
+++ bin/local_storage.rb
@@ -9,8 +9,6 @@ class LocalStorage
       save_to_file(labels_file, array_to_hash(app.labels))
       save_to_file(games_file, array_to_hash(app.games))
       save_to_file(authors_file, array_to_hash(app.authors))
-      save_to_file(games_file, array_to_hash(app.games))
-      save_to_file(authors_file, array_to_hash(app.authors))
       save_to_file(music_albums_file, array_to_hash(app.music_albums))
       save_to_file(genres_file, array_to_hash(app.genres))
     end
@@ -18,30 +16,23 @@ class LocalStorage
     def load_data(app)
       app.labels = load_from_file(labels_file).map { |label| Label.from_hash(label) }
       app.authors = load_from_file(authors_file).map { |author| Author.from_hash(author) }
-      app.authors = load_from_file(authors_file).map { |author| Author.from_hash(author) }
-      app.authors = load_from_file(authors_file).map { |author| Author.from_hash(author) }
       app.genres = load_from_file(genres_file).map { |genre| Genre.from_hash(genre) }
 
       load_books(app)
       load_music_albums(app)
       load_games(app)
-      end
+    end
 
-      def load_books(app)
-        load_books(app)
-      load_music_albums(app)
-      load_games(app)
+    def load_books(app)
+      load_from_file(books_file).map do |book|
+        new_book = Book.from_hash(book)
+        new_label = app.labels.find { |label| label.compare_to?(book['label_id']) }
+        new_book.add_label(new_label) if new_label
+        app.books << new_book
       end
+    end
 
-      def load_books(app)
-        load_from_file(books_file).map do |book|
-            new_book = Book.from_hash(book)
-            new_label = app.labels.find { |label| label.compare_to?(book['label_id']) }
-            new_book.add_label(new_label) if new_label
-            app.books << new_book
-          end
-      end
-
+    def load_music_albums(app)
       load_from_file(music_albums_file).map do |album|
         new_album = MusicAlbum.from_hash(album)
         new_genres = app.genres.find { |genre| genre.compare_to?(album['genre_id']) }
@@ -50,32 +41,14 @@ class LocalStorage
       end
     end
 
-      def load_games(app)
-        load_from_file(games_file).map do |game|
-          new_game = Game.from_hash(game)
-          new_author = app.authors.find { |author| author.compare_to?(game['author_id']) }
-          new_game.add_author(new_author) if new_author
-          app.games << new_game
-        end
+    def load_games(app)
+      load_from_file(games_file).map do |game|
+        new_game = Game.from_hash(game)
+        new_author = app.authors.find { |author| author.compare_to?(game['author_id']) }
+        new_game.add_author(new_author) if new_author
+        app.games << new_game
       end
-
-      def load_games(app)
-        load_from_file(games_file).map do |game|
-          new_game = Game.from_hash(game)
-          new_author = app.authors.find { |author| author.compare_to?(game['author_id']) }
-          new_game.add_author(new_author) if new_author
-          app.games << new_game
-        end
-      end
-
-      def load_games(app)
-        load_from_file(games_file).map do |game|
-          new_game = Game.from_hash(game)
-          new_author = app.authors.find { |author| author.compare_to?(game['author_id']) }
-          new_game.add_author(new_author) if new_author
-          app.games << new_game
-        end
-      end
+    end
 
     def save_to_file(filename, data)
       File.write(filename, JSON.generate(data))
